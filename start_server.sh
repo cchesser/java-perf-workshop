2fb1dc3644171de03144d9f2ce3c9cb817cd7373
AnEmortalKid <janmonterrubio@gmail.com>
2017-08-24 13:06:56 -0500

    adding startup script (#8)
    * Update README.md
    
    * updating gitignore
    
    * Update README.md
    
    typoh fix begging->begin
    
    * convenience running script
    
    * cleanup gitignore

---
+++ start_server.sh
@@ -0,0 +1,3 @@
+echo "==>java -jar $@ java-perf-workshop-server/target/java-perf-workshop-server-1.0-SNAPSHOT.jar server server.yml"
+java -jar $@ java-perf-workshop-server/target/java-perf-workshop-server-1.0-SNAPSHOT.jar server server.yml
+
